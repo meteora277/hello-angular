37dae8b24d5f8495a1c6b77c814a77ba707aefbb
Justin Diaz <justin.s.diaz@gmail.com>
2022-03-19 22:36:58 -0400

    create addNameLocal method

--- src/app/hello-world/hello-world.component.ts
+++ src/app/hello-world/hello-world.component.ts
@@ -3,14 +3,17 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-hello-world',
   template: `
-    <p>hello-world works! {{ count }}</p>
+    <p>hello {{ name }}</p>
     <button type="button" (click)="onClickMe()">uwu</button>
     <button (click)="onClickReset()">Reset</button>
+    <button (click)="addNameLocal('uwu')">Add uwu to local storage</button>
   `,
   styles: [],
 })
 export class HelloWorldComponent implements OnInit {
   count: number = 0;
+  name: string = localStorage.getItem('name') || '';
+
   constructor() {}
 
   onClickMe() {
@@ -19,6 +22,14 @@ export class HelloWorldComponent implements OnInit {
 
   onClickReset() {
     this.count = 0;
+  }
+  onChangeName(name: string) {
+    this.name = name;
+  }
+
+  addNameLocal(name: string) {
+    localStorage.setItem('name', name);
+    this.name = name;
   }
 
   ngOnInit(): void {}
