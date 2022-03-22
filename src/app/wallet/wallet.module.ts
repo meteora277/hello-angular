5408dc79ad0608bb232e2835311d70153314364a
Justin Diaz <justin.s.diaz@gmail.com>
2022-03-22 14:38:39 -0400

    declare purchase component in wallet module

--- src/app/wallet/wallet.module.ts
+++ src/app/wallet/wallet.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WalletComponent } from './wallet.component';
+import { PurchaseComponent } from '../purchase/purchase.component';
 
 @NgModule({
-  declarations: [WalletComponent],
+  declarations: [WalletComponent, PurchaseComponent],
   imports: [CommonModule],
   exports: [WalletComponent],
 })
