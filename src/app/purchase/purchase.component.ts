900a8315072f8f3f0d855e884d94a669d9148908
Justin Diaz <justin.s.diaz@gmail.com>
2022-03-22 15:13:45 -0400

    create make purchase method

--- src/app/purchase/purchase.component.ts
+++ src/app/purchase/purchase.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-purchase',
   templateUrl: './purchase.component.html',
-  styleUrls: ['./purchase.component.scss']
+  styleUrls: ['./purchase.component.scss'],
 })
 export class PurchaseComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  makePurchase(purchase: number) {
+    const wallet: number = Number(localStorage.getItem('wallet'));
+    const updatedWallet: number = wallet - purchase;
+    localStorage.setItem('wallet', `${updatedWallet}`);
   }
 
+  ngOnInit(): void {}
 }
