7ef62e55a5a88d9006773e8eabde58c191ceb497
Justin Diaz <justin.s.diaz@gmail.com>
2022-03-22 14:38:21 -0400

    import / use purchase component

--- src/app/wallet/wallet.component.ts
+++ src/app/wallet/wallet.component.ts
@@ -2,7 +2,10 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-wallet',
-  template: ` <div>\${{ wallet }}</div> `,
+  template: `
+    <div>\${{ wallet }}</div>
+    <app-purchase></app-purchase>
+  `,
   styleUrls: ['./wallet.component.css'],
 })
 export class WalletComponent implements OnInit {
