9c02112e32f5af4e5d02a58da22d9ab7d46b8779
Justin Diaz <justin.s.diaz@gmail.com>
2022-04-25 18:46:00 -0400

    create core module for services

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { WindowRefService } from './window-ref.service';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule],
+  providers: [WindowRefService],
+})
+export class CoreModule {}
